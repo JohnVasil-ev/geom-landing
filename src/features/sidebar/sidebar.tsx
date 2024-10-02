b36ec4c5176e7849cda9dff76f4547abff96e45a
John Vasiliev <va3iliev.ip@gmail.com>
2024-10-02 14:13:00 +0300

    Finished sidebar

--- src/features/sidebar/sidebar.tsx
+++ src/features/sidebar/sidebar.tsx
@@ -54,16 +54,23 @@ export function Sidebar() {
 		};
 	}, [isActive, onSidebarClose]);
 
-	// TODO(IV): Change text in buttons to SVGs
 	return (
 		<>
-			<Button onClick={onSidebarOpen}>Open sidebar</Button>
+			<Button className={style['open-button']} onClick={onSidebarOpen}>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#ffffff" stroke-width="2">
+					<path d="M4 7h16M4 12h16M4 17h16" />
+				</svg>
+			</Button>
 
 			<Portal isActive={isActive}>
 				<div className={style['sidebar-wrapper']}>
 					<div className={style.backdrop} style={{ opacity }} onClick={onSidebarClose} />
 					<div className={style.sidebar} style={{ transform: `translate(${translateX}, 0%)` }}>
-						<Button className={style['close-button']} onClick={onSidebarClose}>X</Button>
+						<Button className={style['close-button']} onClick={onSidebarClose}>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#000000" stroke-width="1">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+							</svg>
+						</Button>
 
 						<div className={style.anchors}>
 							<span className={style.anchor} onClick={() => onAnchorClick(Anchors.Hero)}>
